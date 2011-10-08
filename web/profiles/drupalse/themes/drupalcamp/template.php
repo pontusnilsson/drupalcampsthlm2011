69e24c153414b8f0a137a7825f6d35631322c5ef
Pontus Nilsson <pontus@nodeone.se>
2011-10-08 10:09:33 +0200

    Updates live state

--- web/profiles/drupalse/themes/drupalcamp/template.php
+++ web/profiles/drupalse/themes/drupalcamp/template.php
@@ -10,3 +10,16 @@ function drupalcamp_panels_default_style_render_region($display, $region_id, $pa
   return $output;
 }
 
+
+/**
+ * Views override; field_blurb_image_fid field.
+ *
+ * @see mykea_views_view_field__field_blurb_link_text_value
+ */
+function drupalcamp_views_view_field__sessions__field_speakers_uid($view, $field, $row) {
+  kpr($view);
+  if($field) {
+
+  }
+  return $view->field[$field->options['id']]->advanced_render($row);
+}
